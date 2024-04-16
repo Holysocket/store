d4f38f07132607ba12b1982d31352beab3f73a79
╨Т╨╗╨░╨┤╨╕╨╝╨╕╤А@HOME-PC <ggggaron3@gmail.com>
2024-04-16 20:01:02 +0700

    Описать сущность Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Store;
+
+public class Book
+{
+    public Int Id { get; }
+  public string Title { get; }
+
+    public Book (int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+}
